2816c90ba038fea9da5a9ace0541ebade1288ed7
13822512654 <lix_1234@126.com>
2020-11-07 10:48:20 +0800

    知识点1

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,29 @@
 // pages/home/home.js
+// 必须调用page()并且用对象传入自变量
 Page({
+data:{
+  name:'lix',
+  age:3,
+  students:[
+    {id:1,name:'amy',age:17},
+    {id:2,name:'lily',age:27},
+    {id:3,name:'sam',age:7},
+    {id:4,name:'karni',age:22},
+  ],
+  counter:0
+},
+handleBtnClick(){
+  // 数据发生变化，但是界面没办法监听发生变化，界面不刷新，错误做法
+  // this.data.counter++
+  // console.log( this.data.counter)
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+  this.setData({
+    counter:this.data.counter+1
+  })
+}
 
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
+
+// 编程范式
+//1、命令式编程：js原生操作DOM
+//2、声明式编程：Vue/React/Angular
